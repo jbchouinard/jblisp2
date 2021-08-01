e8b6a12ae511c58eb88d191b25b97231305da1a2
Jerome Boisvert-Chouinard <jerome_boisvertchouinard@trendmicro.com>
2021-07-31 21:17:40 -0400

    trivial REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
+const HISTORY_FILE: &str = ".jblisp2_history";
+
 fn main() {
-    println!("Hello, world!");
+    // `()` can be used when no completer is required
+    let mut rl = Editor::<()>::new();
+    let _ = rl.load_history(HISTORY_FILE);
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                println!("{}", line);
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("^C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("^D");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+    rl.save_history(HISTORY_FILE).unwrap();
 }
